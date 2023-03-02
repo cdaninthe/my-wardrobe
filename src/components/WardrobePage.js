806149d31b8c6654c278b5b00f303d62e437b210
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2023-03-01 19:17:10 -0500

    search component creation

--- src/components/WardrobePage.js
+++ src/components/WardrobePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
 import Items from "./Items";
-
+import Search from "./Search";
 
 function WardrobePage({items, setItems}){
     
@@ -10,6 +10,7 @@ function WardrobePage({items, setItems}){
         <div>
             <br />
             <h3>I have {items.length} items in my wardrobe</h3>
+            <Search />
             <Container>
                 <Items items={items} setItems={setItems}/>
             </Container>
