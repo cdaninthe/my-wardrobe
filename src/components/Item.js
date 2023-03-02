5a800c79bdec3505f6dacf31d23a9a6b7c2a4d09
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2023-03-02 12:35:35 -0500

    update item done

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,8 +4,7 @@ import { Card } from "semantic-ui-react";
 function Item({item, onDeleteItem, onUpdateItem}){
     const [pack, setPack]= useState(false)
     const [isHidden, setHidden]=useState("hidden")
-    const [update, setUpdate]=useState('')
-    const [value, setValue]= useState('')
+    const [newImage, setNewImage]= useState('')
     
     function handleDeleteClick(){
         fetch(`http://localhost:3000/items/${item.id}`, {
@@ -31,27 +30,17 @@ function Item({item, onDeleteItem, onUpdateItem}){
 
     function handleSubmitUpdate(e){
         e.preventDefault()
-        update === "color" ? (
-            fetch(`http://localhost:3000/items/${item.id}`, {
-                method: "PATCH",
-                headers: {
-                    "Content-Type": 'application/json',
-                }, 
-                body: JSON.stringify({
-                    color: value,
-                }),
-            })
-        ) : (
-            fetch(`http://localhost:3000/items/${item.id}`, {
-                method: "PATCH",
-                headers: {
-                    "Content-Type": 'application/json',
-                }, 
-                body: JSON.stringify({
-                    image: value,
-                }),
-            })
-        )
+        
+        fetch(`http://localhost:3000/items/${item.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": 'application/json',
+            }, 
+            body: JSON.stringify({
+                image: newImage,
+            }),
+        })
+        
         .then((r)=> r.json())
         .then((updatedItem)=> onUpdateItem(updatedItem))
         isHidden ? setHidden("") : setHidden("hidden")
@@ -67,19 +56,14 @@ function Item({item, onDeleteItem, onUpdateItem}){
                 <p>Size: {item.size}</p>
                 <p>Color: {item.color}</p>
                 {pack ? 
-                    (<button onClick={handlePackClick}>Packed</button>) 
+                    (<button onClick={handlePackClick} className='active'>Packed</button>) 
                     :
                     (<button onClick={handlePackClick}>Pack for Trip</button>)
                 }
                 <button onClick={handleDeleteClick}>Donate / Declutter</button>
-                <button onClick={handleUpdateClick}>Update Item</button> 
+                <button onClick={handleUpdateClick}>Update Item Image</button> 
                 <form hidden={isHidden} onSubmit={handleSubmitUpdate}>
-                    <select id="update-select" onChange={(e)=> setUpdate(e.target.value)}>
-                        <option value="">--Choose your update--</option>
-                        <option value="color">Update Color</option>
-                        <option value="image">Update Image</option>
-                    </select>
-                    <input onChange={(e)=> setValue(e.target.value)}></input>
+                    <input placeholder="insert new image..." onChange={(e)=> setNewImage(e.target.value)}></input>
                     <button type="submit">Submit Update</button>
                 </form>             
             </div>
